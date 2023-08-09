ae0ea92be57e110913c805ea400bca72869ae652
HadalyVillasclaras <hadalyvf@gmail.com>
2023-08-09 13:35:24 +0200

    Updates

---
+++ icons/Icons.tsx
@@ -0,0 +1,15 @@
+import { ReactComponent as ArrowLeftIcon } from "./arrow-left.svg";
+import { ReactComponent as ArrowRightIcon } from "./arrow-right.svg";
+import { ReactComponent as PlusIcon } from "./plus.svg";
+import { ReactComponent as LessIcon } from "./less.svg";
+import { ReactComponent as XIcon } from "./x.svg";
+
+const icons = {
+  arrowLeft: ArrowLeftIcon,
+  arrowRight: ArrowRightIcon,
+  plus: PlusIcon,
+  less: LessIcon,
+  x: XIcon
+};
+
+export default icons;
